f5fe65a2eab1b5d493ffa638e79ced651ecc6c40
Stepan Vodyanov <stepan.vodnyy@gmail.com>
2025-05-10 23:06:34 +0400

    refactor models_metrics

--- adversarial/model_metrics.py
+++ adversarial/model_metrics.py
@@ -1,92 +1,93 @@
 import torch
-from yolov5.utils.metrics import ap_per_class
-from yolov5.utils.general import non_max_suppression, scale_boxes, check_dataset, xywh2xyxy, box_iou, coco80_to_coco91_class
+from yolov5.utils.metrics import ap_per_class, box_iou
+from yolov5.utils.general import non_max_suppression, scale_boxes, check_dataset, xywh2xyxy
 from yolov5.utils.dataloaders import create_dataloader
 import numpy as np
-import os
 
 
-def evaluate_yolov5(model, data_path, conf_thres, iou_thres, device, selected_classes):
+def filter_pred_by_classes(pred, selected_classes, device):
+    class_probs = pred[..., 5:]
+    class_ids = class_probs.argmax(dim=2)
+    selected_classes_tensor = torch.tensor(selected_classes, device=device)
+    mask = torch.zeros_like(class_ids, dtype=torch.bool)
+    for cls in selected_classes_tensor:
+        mask |= (class_ids == cls)
+    pred_filtered = []
+    batch_size = pred.shape[0]
+    for i in range(batch_size):
+        pred_i = pred[i]
+        mask_i = mask[i]
+        pred_filtered.append(pred_i[mask_i])
+    return pred_filtered
+
+
+def evaluate_yolov5(model, data_path, conf_thres, iou_thres, device, selected_classes = None):
     model.eval()
     data = check_dataset(data_path)
     dataloader, dataset = create_dataloader(data['test'], imgsz=640, batch_size=8, stride=model.stride, rect=True, prefix='test: ')
-
-    iouv = torch.linspace(0.5, 0.95, 10).to(device)  # IoU от 0.5 до 0.95
+    iouv = torch.linspace(0.5, 0.95, 10).to(device)
     niou = iouv.numel()
     stats = []
 
-
     for batch_i, (img, targets, paths, shapes) in enumerate(dataloader):
-        img = img.to(device, non_blocking=True)
+        img = img.to(device, non_blocking=True).float() / 255.0
         targets = targets.to(device)
-        with torch.no_grad():
-            pred = model(img)  # Получаем все выходы
-            if isinstance(pred, (list, tuple)):
-                pred = pred[0]  # Берем основной тензор предсказаний
-        pred = non_max_suppression(pred, conf_thres, iou_thres)
-        for si, pred in enumerate(pred):
-            labels = targets[targets[:, 0] == si, 1:]
-            nl = len(labels)
-            tcls = labels[:, 0].tolist() if nl else []
-            if len(pred) == 0:
-                if nl:
-                    stats.append((torch.zeros(0, niou, dtype=torch.bool),
-                                torch.Tensor(), torch.Tensor(), tcls))
-                continue
-            # Фильтрация по выбранным классам
-            if selected_classes is not None:
-                pred = pred[np.isin(pred[:, 5].cpu().numpy(), selected_classes)]
-                if len(pred) == 0:
-                    if nl:
-                        stats.append((torch.zeros(0, niou, dtype=torch.bool),
-                                      torch.Tensor(), torch.Tensor(), tcls))
-                    continue
 
-            predn = pred.clone()  # копируем предсказания
-            scale_boxes(img[si].shape[1:], predn[:, :4], shapes[si][0], shapes[si][1])
-            correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool, device=device)
-            if nl:
-                detected = []  # индексы уже сопоставленных GT
-                tcls_tensor = labels[:, 0]
-                # Конвертация GT в xyxy формат
+        with torch.no_grad():
+            pred = model(img)
+            if isinstance(pred, (list, tuple)):
+                pred = pred[0]
+
+        if selected_classes is not None:
+            pred = filter_pred_by_classes(pred, selected_classes, device)
+        else:
+            pred = [pred[i] for i in range(pred.shape[0])]
+
+        pred = [non_max_suppression(p.unsqueeze(0), conf_thres, iou_thres)[0] for p in pred]
+
+        for si, pred_i  in enumerate(pred):
+            batch_idx = targets[:, 0] == si
+            labels = targets[batch_idx, 1:]
+            gt_num = len(labels)
+            gt_cls = labels[:, 0].tolist() if gt_num else []
+
+            if len(pred_i) == 0:
+                if gt_num:
+                    stats.append((torch.zeros(0, niou, dtype=torch.bool),
+                                  torch.Tensor(), torch.Tensor(), gt_cls))
+                continue
+
+            predn = pred_i.clone()
+            img0_shape = tuple(shapes[si][0])
+            ratio_pad = (tuple(shapes[si][1][0]), tuple(shapes[si][1][1]))
+            scale_boxes(img[si].shape[1:], predn[:, :4], img0_shape, ratio_pad)
+
+            correct = torch.zeros(predn.shape[0], niou, dtype=torch.bool, device=device)
+            if gt_num:
+                detected = []
+                gt_cls_tensor = labels[:, 0]
                 tbox = xywh2xyxy(labels[:, 1:5])
-                scale_boxes(img[si].shape[1:], tbox, shapes[si][0], shapes[si][1])  # масштабирование GT
-                for j in range(nl):
+                tbox[:, 0] *= img0_shape[1]
+                tbox[:, 1] *= img0_shape[0]
+                tbox[:, 2] *= img0_shape[1]
+                tbox[:, 3] *= img0_shape[0]
+                for j in range(gt_num):
                     iou = box_iou(predn[:, :4], tbox[j].unsqueeze(0))
                     iou, i = iou.max(0)
-
-                    print(f"\nОбработка GT {j}:")
-                    print(f"Класс GT: {tcls_tensor[j].item()}")
-                    print(f"Координаты GT: {tbox[j]}")
-                    print(f"Лучшее совпадение - pred {i}: класс {pred[i, 5].item()}, IoU {iou.item()}")
-                    print(f"Уже обнаружено: {i in detected}")
-
-                    if iou >= iouv[0]:
-                        print("IoU >= порога")
-                        if pred[i, 5] == tcls_tensor[j]:
-                            print("Классы совпадают")
-                            if i not in detected:
-                                print("Новое обнаружение!")
-                                correct[i] = iou >= iouv
-                                detected.append(i)
-                        else:
-                            print("Классы не совпадают")
-                    else:
-                        print("IoU ниже порога")
-
-                    if iou >= iouv[0] and pred[i, 5] == tcls_tensor[j] and i not in detected:
-                        correct[i] = iou >= iouv  # правильное для всех порогов IoU
+                    if iou >= iouv[0] and predn[i, 5] == gt_cls_tensor[j] and i not in detected:
+                        correct[i] = iou >= iouv
                         detected.append(i)
-            stats.append((correct.cpu(),
-                         pred[:, 4].cpu(),  # confidence
-                         pred[:, 5].cpu(),  # predicted class
-                         tcls))  # target class
 
-    stats = [np.concatenate(x, 0) for x in zip(*stats)]
+            stats.append((correct.cpu(),
+                          predn[:, 4].cpu(),  # confidence
+                          predn[:, 5].cpu(),  # predicted class
+                          gt_cls))  # target class
+
+    stats = [np.concatenate(x, 0) for x in zip(*stats)] if stats else [np.array([])]*4
     if len(stats) and stats[0].any():
-        tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats)
+        tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats, names=model.names)
         ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
-        mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+        mp, mr, map50, mAP = p.mean(), r.mean(), ap50.mean(), ap.mean()
 
         # Фильтрация результатов по выбранным классам
         if selected_classes is not None:
@@ -96,7 +97,7 @@ def evaluate_yolov5(model, data_path, conf_thres, iou_thres, device, selected_cl
             p, r = p[mask], r[mask]  # Фильтруем precision и recall
             print(f"Оценка только для классов: {ap_class.tolist()}")
 
-        print(f"mAP@0.5: {map50:.4f}, mAP@0.5:0.95: {map:.4f}")
+        print(f"P: {p}, R: {r}, mAP@0.5: {map50:.4f}, mAP@0.5:0.95: {mAP:.4f}")
 
         # Создаем словарь с метриками для каждого класса
         class_metrics = {}
@@ -112,7 +113,7 @@ def evaluate_yolov5(model, data_path, conf_thres, iou_thres, device, selected_cl
         # Возвращаем все метрики
         return {
             'map50': map50,
-            'map': map,
+            'map': mAP,
             'class_metrics': class_metrics,
             'global_precision': mp,
             'global_recall': mr
@@ -133,8 +134,10 @@ def main():
     test_images_dir = "../adversarial/dataset/test/images"
     conf_thres = 0.3
     iou_thres = 0.5
-    model_p = torch.hub.load('../yolov5', 'yolov5s', source='local', pretrained=True).eval()
-    evaluate_yolov5(model_p,'../adversarial/dataset/data.yaml', conf_thres, iou_thres, device, [0])
+    # model_p = torch.hub.load('../yolov5', 'yolov5s', source='local', pretrained=True).eval()
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, autoshape=False)
+    metrics = evaluate_yolov5(model, '../adversarial/dataset/data.yaml', conf_thres, iou_thres, device)
+    print()
 
 
 if __name__ == "__main__":
